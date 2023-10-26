c47ea1822319672feb46fedb53b0bf989afe957c
it21155352 <it21155352@my.sliit.lk>
2023-10-26 11:06:04 +0530

    complete home page

--- app/src/main/java/com/example/car_parking/HomedashboredUser.kt
+++ app/src/main/java/com/example/car_parking/HomedashboredUser.kt
@@ -1,11 +1,37 @@
 package com.example.car_parking
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class HomedashboredUser : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homedashbored_user)
+       var maps = findViewById<ImageView>(R.id.mapview)
+        var bookingim =findViewById<ImageView>(R.id.bokinggo)
+        var btnbook = findViewById<Button>(R.id.regidterbtn)
+
+        btnbook.setOnClickListener {
+            val os = Intent(this,vehiclBokkin::class.java)
+
+            startActivity(os)
+        }
+        maps.setOnClickListener {
+            val mapsAct = Intent(this,MapsActivity::class.java)
+
+            startActivity(mapsAct)
+
+        }
+
+        bookingim.setOnClickListener {
+            val bookinAct = Intent(this,vehiclBokkin::class.java)
+
+            startActivity(bookinAct)
+        }
     }
 }
