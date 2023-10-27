579f9005255b6dd868d8a720d1ae04c40027d52b
it21155352 <it21155352@my.sliit.lk>
2023-10-27 16:12:16 +0530

    modify home page

--- app/src/main/java/com/example/car_parking/homedashboredUser.kt
+++ app/src/main/java/com/example/car_parking/homedashboredUser.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 
 class homedashboredUser : AppCompatActivity() {
@@ -15,7 +16,13 @@ class homedashboredUser : AppCompatActivity() {
        var mapsvs = findViewById<ImageView>(R.id.mapview)
         var bookingim =findViewById<ImageView>(R.id.bokinggo)
         var btnbook = findViewById<Button>(R.id.reciverplace)
+        var find = findViewById<EditText>(R.id.findg)
 
+        find.setOnClickListener {
+            val mapsAct = Intent(this, mapsActivity::class.java)
+
+            startActivity(mapsAct)
+        }
         btnbook.setOnClickListener {
             val os = Intent(this, vehiclBokkin::class.java)
 
